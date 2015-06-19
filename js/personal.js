57f0d632f8a7b1d9c99e5b4ee930d1536c113881
raul26 <k_vm26@hotmail.com>
2015-06-19 12:14:30 -0500

    working on js

--- js/personal.js
+++ js/personal.js
@@ -11,6 +11,7 @@ function selectBackground (opt) {
   switch(opt) {
     case 1:
       dir='css/images/H1.png';
+      color='#949c39';
     break;
     case 2:
       dir='css/images/H2.png';
@@ -18,21 +19,27 @@ function selectBackground (opt) {
     break;
     case 3:
       dir='css/images/H3.png';
+      color='#d68585';
     break;
     case 4:
       dir='css/images/H4.png';
+      color='#cc9966';
     break;
     case 5:
       dir='css/images/H5.png';
+      color='#99cc99';
     break;
     case 6:
       dir='css/images/H6.png';
+      color='#9999cc';
     break;
     case 7:
       dir='css/images/H7.png';
+      color='#669999';
     break;
     case 8:
       dir='css/images/H8.png';
+      color='#999999';
     break;
   }
   $("body").css("backgroundColor", color);
