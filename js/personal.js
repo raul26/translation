7ff5bab39d481b56ec58a487d09af2bfa2605cb8
raul26 <k_vm26@hotmail.com>
2015-06-21 18:35:00 -0500

    working

--- js/personal.js
+++ js/personal.js
@@ -10,63 +10,11 @@ $(document).ready(function(){
 		$(this).children().last().animate({ width: "90%" }, 1000 );
     $(this).addClass("selected");
 	})
-function selectBackground (opt) {
-  var dir='';
-  var color='';
-  var content='';
-  switch(opt) {
-    case 1:
-      dir='css/images/H1.png';
-      color='#949c39';
-      content='#uno';
-    break;
-    case 2:
-      dir='css/images/H2.png';
-      color='#4d9e99';
-      content='#dos';
-    break;
-    case 3:
-      dir='css/images/H3.png';
-      color='#d68585';
-      content='#tres';
-    break;
-    case 4:
-      dir='css/images/H4.png';
-      color='#cc9966';
-      content='#cuatro';
-    break;
-    case 5:
-      dir='css/images/H5.png';
-      color='#99cc99';
-      content='#cinco';
-    break;
-    case 6:
-      dir='css/images/H6.png';
-      //color='#9999cc';
-      content='#seis';
-    break;
-    case 7:
-      dir='css/images/H7.png';
-      //color='#669999';
-      content='#siete';
-    break;
-    case 8:
-      dir='css/images/H8.png';
-      color='#999999';
-      content='#ocho';
-    break;
-  }
-  $('.content').hide();
-  $(content).show();
-  $("body").css("backgroundColor", color);
-  $("body").css("backgroundImage", "url(" + dir + ")");
-}
-
-
-
-
-
-
-
 });
-
+function selectBackground (opt) {
+  $('.content').hide();
+  $("body").removeClass("cl-uno cl-dos cl-tres cl-cuatro cl-cinco cl-seis cl-siete cl-ocho");
+  $("body").addClass(opt);
+  var id = '#'+opt.split('-')[1];
+  $(id).show();
+}
